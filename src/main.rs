4618abce897dd186f6590374fdf1bce3bbf49e8d
Adam Gausmann <agausmann@fastmail.com>
2022-03-13 21:07:03 -0500

    Add disk/network pages, and add memory to system page

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,38 @@ pub mod config;
 use std::time::{Duration, Instant};
 
 use anyhow::Context;
-use cfa635::{Key, Report};
+use cfa635::{Key, Report, NUM_COLUMNS};
 use config::Config;
 use sysinfo::{System, SystemExt};
 
 const POLL_INTERVAL: Duration = Duration::from_millis(10);
 const REFRESH_INTERVAL: Duration = Duration::from_secs(2);
 const SCREEN_TIMEOUT: Duration = Duration::from_secs(15);
+
+#[derive(Debug, Clone, Copy)]
+enum Page {
+    System,
+    Disk,
+    Network,
+}
+
+impl Page {
+    fn next(&self) -> Self {
+        match self {
+            Self::System => Self::Disk,
+            Self::Disk => Self::Network,
+            Self::Network => Self::System,
+        }
+    }
+
+    fn prev(&self) -> Self {
+        match self {
+            Self::Disk => Self::System,
+            Self::Network => Self::Disk,
+            Self::System => Self::Network,
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let config_raw =
@@ -35,6 +60,7 @@ fn main() -> anyhow::Result<()> {
     let mut next_poll = Instant::now();
     let mut next_refresh = Instant::now();
     let mut screen_timeout = Some(Instant::now());
+    let mut current_page = Page::System;
 
     loop {
         let now = Instant::now();
@@ -78,18 +104,34 @@ fn main() -> anyhow::Result<()> {
                 next_refresh += REFRESH_INTERVAL;
             }
             if screen_timeout.is_some() {
-                system.refresh_cpu();
-
-                lcd.clear_screen()?;
                 if let Some(name) = system.host_name() {
-                    lcd.set_text(0, 0, name.as_bytes())?;
+                    lcd.set_text(0, 0, &pad_line(name))?;
                 }
-                let load_avg = system.load_average();
-                let load_avg_str = format!(
-                    "{:.2} {:.2} {:.2}",
-                    load_avg.one, load_avg.five, load_avg.fifteen,
-                );
-                lcd.set_text(1, 0, load_avg_str.as_bytes())?;
+
+                match current_page {
+                    Page::System => {
+                        system.refresh_cpu();
+                        system.refresh_memory();
+
+                        let load_avg = system.load_average();
+                        let load_avg_str = format!(
+                            "CPU: {:.2} {:.2} {:.2}",
+                            load_avg.one, load_avg.five, load_avg.fifteen,
+                        );
+                        lcd.set_text(1, 0, &pad_line(load_avg_str))?;
+
+                        let total = kb_to_mib(system.total_memory());
+                        let unavailable = total - kb_to_mib(system.available_memory());
+                        let memory_str = format!("Mem: {}/{} M", unavailable, total);
+                        lcd.set_text(2, 0, &pad_line(memory_str))?;
+                    }
+                    Page::Disk => {
+                        system.refresh_disks_list();
+                    }
+                    Page::Network => {
+                        system.refresh_networks_list();
+                    }
+                }
             }
         }
 
@@ -102,4 +144,17 @@ fn main() -> anyhow::Result<()> {
 
         std::thread::sleep(Duration::from_millis(10));
     }
+}
+
+fn pad_line<V>(vec: V) -> Vec<u8>
+where
+    V: Into<Vec<u8>>,
+{
+    let mut vec = vec.into();
+    vec.resize(NUM_COLUMNS as usize, b' ');
+    vec
+}
+
+fn kb_to_mib(x: u64) -> u64 {
+    x * 1024 / 1000 / 1024
 }
