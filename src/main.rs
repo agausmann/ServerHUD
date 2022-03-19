ce5117b8d7b88b49e3ede0a87da979b477e725d9
Adam Gausmann <agausmann@fastmail.com>
2022-03-19 01:30:01 -0500

    Refactor app state into a struct with methods

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,249 @@ const POLL_INTERVAL: Duration = Duration::from_millis(10);
 const REFRESH_INTERVAL: Duration = Duration::from_secs(2);
 const SCREEN_TIMEOUT: Duration = Duration::from_secs(15);
 
+struct App {
+    config: Config,
+    system: System,
+    next_poll: Instant,
+    next_refresh: Instant,
+    screen_timeout: Option<Instant>,
+    lcd: cfa635::Device,
+    current_page: Page,
+    scroll: usize,
+    max_scroll: Option<usize>,
+    buffer: [[u8; NUM_COLUMNS as usize]; NUM_ROWS as usize],
+}
+
+impl App {
+    fn new(config: Config) -> anyhow::Result<Self> {
+        let mut lcd =
+            cfa635::Device::new(&config.lcd.path).context("failed to open LCD serial port")?;
+        lcd.configure_key_reporting(
+            &[
+                Key::Up,
+                Key::Down,
+                Key::Left,
+                Key::Right,
+                Key::Enter,
+                Key::Exit,
+            ],
+            &[],
+        )?;
+        let system = System::new();
+        let now = Instant::now();
+        Ok(Self {
+            config,
+            system,
+            next_poll: now,
+            next_refresh: now,
+            screen_timeout: Some(now),
+            lcd,
+            current_page: Page::System,
+            scroll: 0,
+            max_scroll: None,
+            buffer: [[b' '; NUM_COLUMNS as usize]; NUM_ROWS as usize],
+        })
+    }
+
+    fn run(mut self) -> anyhow::Result<()> {
+        loop {
+            let now = Instant::now();
+            if now >= self.next_poll {
+                self.poll()?;
+                while now >= self.next_poll {
+                    self.next_poll += POLL_INTERVAL;
+                }
+            }
+
+            if let Some(screen_timeout) = self.screen_timeout {
+                if now >= screen_timeout {
+                    self.sleep()?;
+                }
+            }
+
+            // Make a new "now" that is after the poll,
+            // so if queue_refresh was called, we immediately refresh:
+            let now = Instant::now();
+            if self.is_awake() && now >= self.next_refresh {
+                self.refresh()?;
+                while now >= self.next_refresh {
+                    self.next_refresh += REFRESH_INTERVAL;
+                }
+            }
+
+            std::thread::sleep(Duration::from_millis(10));
+        }
+    }
+
+    fn poll(&mut self) -> anyhow::Result<()> {
+        while let Some(report) = self.lcd.poll_report()? {
+            match report {
+                Report::KeyActivity { key, pressed } if pressed => {
+                    if !self.wake() {
+                        match key {
+                            Key::Left => {
+                                self.current_page = self.current_page.prev();
+                                self.scroll = 0;
+                                self.max_scroll = None;
+                                self.queue_refresh();
+                            }
+                            Key::Right => {
+                                self.current_page = self.current_page.next();
+                                self.scroll = 0;
+                                self.max_scroll = None;
+                                self.queue_refresh();
+                            }
+                            Key::Up => {
+                                if self.scroll > 0 {
+                                    self.scroll -= 1;
+                                    self.queue_refresh();
+                                }
+                            }
+                            Key::Down => {
+                                if let Some(max_scroll) = self.max_scroll {
+                                    if self.scroll < max_scroll {
+                                        self.scroll += 1;
+                                        self.queue_refresh();
+                                    }
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
+
+    fn sleep(&mut self) -> anyhow::Result<()> {
+        self.screen_timeout = None;
+        self.lcd.set_backlight(0, 0)?;
+        Ok(())
+    }
+
+    fn wake(&mut self) -> bool {
+        let was_asleep = self.screen_timeout.is_none();
+        self.screen_timeout = Some(Instant::now() + SCREEN_TIMEOUT);
+        if was_asleep {
+            // Instead of turning on backlight here,
+            // it is deferred until the end of refresh after the LCD is updated.
+            // Otherwise, the old contents might be briefly displayed.
+            self.queue_refresh();
+        }
+        was_asleep
+    }
+
+    fn is_awake(&self) -> bool {
+        self.screen_timeout.is_some()
+    }
+
+    fn queue_refresh(&mut self) {
+        self.next_refresh = Instant::now();
+    }
+
+    fn refresh(&mut self) -> anyhow::Result<()> {
+        self.clear();
+        if let Some(name) = self.system.host_name() {
+            self.set_text(0, 0, name.as_bytes());
+        }
+
+        match self.current_page {
+            Page::System => {
+                self.system.refresh_cpu();
+                self.system.refresh_memory();
+
+                let load_avg = self.system.load_average();
+                let load_avg_str = format!(
+                    "CPU: {:.2} {:.2} {:.2}",
+                    load_avg.one, load_avg.five, load_avg.fifteen,
+                );
+                self.set_text(1, 0, load_avg_str.as_bytes());
+
+                let total = kb_to_mib(self.system.total_memory());
+                let unavailable = total - kb_to_mib(self.system.available_memory());
+                let memory_str = format!("Mem: {}/{} M", unavailable, total);
+                self.set_text(2, 0, memory_str.as_bytes());
+            }
+            Page::Disk => {
+                self.system.refresh_disks_list();
+                self.max_scroll = Some(self.system.disks().len() - (NUM_ROWS as usize - 1));
+                self.scroll = self.scroll.min(self.max_scroll.unwrap());
+
+                let disks = self.system.disks();
+                let sorted_disks = disks
+                    .iter()
+                    .map(|disk| (disk.mount_point().to_string_lossy().into_owned(), disk))
+                    .collect::<BTreeMap<_, _>>();
+
+                let display_disks: Vec<&Disk> = if self.config.disk.paths.is_empty() {
+                    sorted_disks.values().copied().collect()
+                } else {
+                    self.config
+                        .disk
+                        .paths
+                        .iter()
+                        .flat_map(|key| sorted_disks.get(key).copied())
+                        .collect()
+                };
+
+                // Creating an intermediate collection; otherwise, we would
+                // be calling `set_text` while `system` is still borrowed.
+                //
+                // Long term fix: create a struct wrapping the buffer that
+                // provides the set_text method, so set_text can be a disjoint
+                // borrow instead of borrowing all of `self`.
+                let lines: Vec<String> = display_disks
+                    .into_iter()
+                    .skip(self.scroll)
+                    .take(NUM_ROWS as usize - 1)
+                    .map(|disk| {
+                        let total = disk.total_space() >> 30;
+                        let unavailable = total.saturating_sub(disk.available_space() >> 30);
+                        format!(
+                            "{} {}/{} G",
+                            disk.mount_point().to_string_lossy(),
+                            unavailable,
+                            total
+                        )
+                    })
+                    .collect();
+
+                for (i, line) in lines.into_iter().enumerate() {
+                    self.set_text(i + 1, 0, line.as_bytes());
+                }
+            }
+            Page::Network => {
+                self.system.refresh_networks_list();
+                // max_scroll = Some(system.networks().len());
+                // scroll = scroll.min(max_scroll.unwrap());
+            }
+        }
+        self.flush()?;
+        // Deferred backlight control from wake():
+        self.lcd.set_backlight(100, 100)?;
+        Ok(())
+    }
+
+    fn clear(&mut self) {
+        for row in &mut self.buffer {
+            row.fill(b' ');
+        }
+    }
+
+    fn set_text(&mut self, row: usize, col: usize, text: &[u8]) {
+        self.buffer[row][col..][..text.len()].copy_from_slice(text);
+    }
+
+    fn flush(&mut self) -> anyhow::Result<()> {
+        for (row_index, row_text) in self.buffer.iter().enumerate() {
+            self.lcd.set_text(row_index as u8, 0, row_text)?;
+        }
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Page {
     System,
@@ -44,169 +287,9 @@ fn main() -> anyhow::Result<()> {
         std::fs::read("ServerHUD.toml").context("cannot read config file ServerHUD.toml")?;
     let config: Config = toml::from_slice(&config_raw).context("cannot parse config file")?;
 
-    let mut lcd =
-        cfa635::Device::new(&config.lcd.path).context("failed to open LCD serial port")?;
-    lcd.configure_key_reporting(
-        &[
-            Key::Up,
-            Key::Down,
-            Key::Left,
-            Key::Right,
-            Key::Enter,
-            Key::Exit,
-        ],
-        &[],
-    )?;
-
-    let mut system = System::new();
-
-    let mut next_poll = Instant::now();
-    let mut next_refresh = Instant::now();
-    let mut screen_timeout = Some(Instant::now());
-    let mut current_page = Page::System;
-    let mut scroll: usize = 0;
-    let mut max_scroll: Option<usize> = None;
-
-    loop {
-        let now = Instant::now();
-
-        if now > next_poll {
-            while now > next_poll {
-                next_poll += POLL_INTERVAL;
-            }
-
-            while let Some(report) = lcd.poll_report()? {
-                match report {
-                    Report::KeyActivity { key, pressed } if pressed => {
-                        if screen_timeout.is_none() {
-                            lcd.set_backlight(100, 100)?;
-                            // Force refresh
-                            next_refresh = now;
-                        } else {
-                            match key {
-                                Key::Left => {
-                                    current_page = current_page.prev();
-                                    scroll = 0;
-                                    max_scroll = None;
-                                    next_refresh = now;
-                                    lcd.clear_screen()?;
-                                }
-                                Key::Right => {
-                                    current_page = current_page.next();
-                                    scroll = 0;
-                                    max_scroll = None;
-                                    next_refresh = now;
-                                    lcd.clear_screen()?;
-                                }
-                                Key::Up => {
-                                    if scroll > 0 {
-                                        scroll -= 1;
-                                        next_refresh = now;
-                                    }
-                                }
-                                Key::Down => {
-                                    if let Some(max_scroll) = max_scroll {
-                                        scroll = (scroll + 1).min(max_scroll);
-                                        next_refresh = now;
-                                    }
-                                }
-                                _ => {}
-                            }
-                        }
-                        screen_timeout = Some(now + SCREEN_TIMEOUT);
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        if now >= next_refresh {
-            while now > next_refresh {
-                next_refresh += REFRESH_INTERVAL;
-            }
-            if screen_timeout.is_some() {
-                if let Some(name) = system.host_name() {
-                    lcd.set_text(0, 0, &BLANK_LINE)?;
-                    lcd.set_text(0, 0, name.as_bytes())?;
-                }
-
-                match current_page {
-                    Page::System => {
-                        system.refresh_cpu();
-                        system.refresh_memory();
-
-                        let load_avg = system.load_average();
-                        let load_avg_str = format!(
-                            "CPU: {:.2} {:.2} {:.2}",
-                            load_avg.one, load_avg.five, load_avg.fifteen,
-                        );
-                        lcd.set_text(1, 0, &BLANK_LINE)?;
-                        lcd.set_text(1, 0, load_avg_str.as_bytes())?;
-
-                        let total = kb_to_mib(system.total_memory());
-                        let unavailable = total - kb_to_mib(system.available_memory());
-                        let memory_str = format!("Mem: {}/{} M", unavailable, total);
-                        lcd.set_text(2, 0, &BLANK_LINE)?;
-                        lcd.set_text(2, 0, memory_str.as_bytes())?;
-                    }
-                    Page::Disk => {
-                        system.refresh_disks_list();
-                        max_scroll = Some(system.disks().len() - (NUM_ROWS as usize - 1));
-                        scroll = scroll.min(max_scroll.unwrap());
-
-                        let disks = system.disks();
-                        let sorted_disks = disks
-                            .iter()
-                            .map(|disk| (disk.mount_point().to_string_lossy().into_owned(), disk))
-                            .collect::<BTreeMap<_, _>>();
-                        println!("{:?}", sorted_disks);
-
-                        let display_disks: Vec<&Disk> = if config.disk.paths.is_empty() {
-                            sorted_disks.values().copied().collect()
-                        } else {
-                            config
-                                .disk
-                                .paths
-                                .iter()
-                                .flat_map(|key| sorted_disks.get(key).copied())
-                                .collect()
-                        };
-
-                        for (i, disk) in display_disks.into_iter().skip(scroll).take(3).enumerate()
-                        {
-                            let total = disk.total_space() >> 30;
-                            let unavailable = total.saturating_sub(disk.available_space() >> 30);
-                            let disk_str = format!(
-                                "{} {}/{} G",
-                                disk.mount_point().to_string_lossy(),
-                                unavailable,
-                                total
-                            );
-                            lcd.set_text(i as u8 + 1, 0, &BLANK_LINE)?;
-                            lcd.set_text(i as u8 + 1, 0, disk_str.as_bytes())?;
-                        }
-                    }
-                    Page::Network => {
-                        system.refresh_networks_list();
-                        // max_scroll = Some(system.networks().len());
-                        // scroll = scroll.min(max_scroll.unwrap());
-                    }
-                }
-            }
-        }
-
-        if let Some(timeout) = screen_timeout {
-            if now > timeout {
-                lcd.set_backlight(0, 0)?;
-                screen_timeout = None;
-            }
-        }
-
-        std::thread::sleep(Duration::from_millis(10));
-    }
+    let app = App::new(config)?;
+    app.run()
 }
-
-const BLANK_LINE: [u8; NUM_COLUMNS as usize] = [b' '; NUM_COLUMNS as usize];
 
 fn kb_to_mib(x: u64) -> u64 {
     x * 1024 / 1000 / 1024
